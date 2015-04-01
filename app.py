7ae86258efa85343b8486413e0192d997ebf9c0b
asaka <aisk1988@gmail.com>
2015-04-01 13:54:51 +0800

    add print in app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ def application(environ, start_response):
     status = '200 OK'
     output = 'Hello World!'
 
+    print 'xxx'
+
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(output)))
